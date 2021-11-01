e345e0f46ad33b91b9ede0de66bfc2bff45087d0
Galdino <mateusgaldino1000@hotmail.com>
2021-11-01 17:34:45 -0300

    Finish content/hero section on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👌 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>form $9.90 monthly</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="ig.news image" />
+      </main>
     </>
   );
 }
