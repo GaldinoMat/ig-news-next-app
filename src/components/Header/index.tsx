50ab65f59f8f352fcf0cd8e49d8a50abaa33dafc
Galdino <mateusgaldino1000@hotmail.com>
2021-11-23 20:25:49 -0300

    Add subscription event listener to add user to subscription's db

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { SignInButton } from "../SignInButton";
 
 import styles from "./styles.module.scss";
@@ -7,7 +8,9 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width="110" height="30" />
+        <Link href="/" passHref>
+          <Image src="/images/logo.svg" alt="ig.news" width="110" height="30" />
+        </Link>
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
