42eea0a6078bb1d06514a6207016c3e75f1f88cc
Galdino <mateusgaldino1000@hotmail.com>
2021-11-24 15:31:54 -0300

    Add header links as separate components with cloneElement

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/dist/client/router";
+import Link, { LinkProps } from "next/link";
+import { ReactElement, cloneElement } from "react";
+
+interface IActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClass: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClass,
+  ...props
+}: IActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === props.href ? activeClass : "";
+
+  return (
+    <Link {...props}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,25 @@
+import { useRouter } from "next/dist/client/router";
 import Image from "next/image";
 import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 import { SignInButton } from "../SignInButton";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Link href="/">
-          <a>
-            <Image
-              src="/images/logo.svg"
-              alt="ig.news"
-              width="110"
-              height="30"
-            />
-          </a>
-        </Link>
+        <Image src="/images/logo.svg" alt="ig.news" width="110" height="30" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink activeClass={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClass={styles.active} href="/posts">
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
